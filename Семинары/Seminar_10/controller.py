6723c64f298585f221079724a4ace4d0dc21b52d
alexiussev1 <alexius.sev@gmail.com>
2023-04-12 00:46:47 +0300

    смотрю семина

--- Семинары/Seminar_10/controller.py
+++ Семинары/Seminar_10/controller.py
@@ -1,17 +1,22 @@
-import view
+import view, model, text_fields as txt
 
 def start_pb():
     while True:
         choise = view.main_menu()
         match choise:
             case 1:
-                pass
+                model.load_file()
+                view.print_info(txt.load_success_full)
             case 2:
-                pass
+                model.save_file()
+                view.print_info(txt.save_success_full)
             case 3:
-                pass
+                pb= model.get_pb()
+                view.show_contacts(pb, txt.no_contact_or_file)
             case 4:
-                pass
+                contact = view.new_contact
+                model.add_contacts(contact)
+                view.print_info(txt.new_contact)
             case 5:
                 pass
             case 6:
@@ -20,4 +25,4 @@ def start_pb():
                 pass
             case 8:
                 pass
-            # case _: если ве другое не сработало
+            # case _: если вcе другое не сработало
--- Семинары/Seminar_10/model.py
+++ Семинары/Seminar_10/model.py
@@ -1,4 +1,27 @@
 phone_book =[]
-PATH = ''
+PATH = 'Семинары\Seminar_10\phone_book.txt'
+
+def get_pb():
+    return phone_book
 
 def load_file():
+    with open(PATH, 'r', encoding='UTF-8') as file:
+        data = file.readline()
+    for contact in data:
+        contact = contact.strip().split(';')
+        phone_book.append({'name': contact[0],
+                           'phone': contact[1],
+                           'comment': contact[2]})
+        
+def save_file():
+    data = []
+    for contact in phone_book:
+        data.append(';'.join([value for value in contact.value()]))
+    data = '\n'.doin(data)
+    with open (PATH, 'w', encoding='UTF-8') as file:
+        file.write(data)
+
+def add_contacts(contact: dict):
+    global phone_book
+    phone_book.append(contact)
+
